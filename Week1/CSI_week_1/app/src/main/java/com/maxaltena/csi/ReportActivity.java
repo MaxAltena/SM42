31fd45908eecd4ebe0105e8ef69f554298fcabdb
MaxAltena <Max@MaxAltena.com>
2019-02-08 18:00:03 +0100

    did some more stuff this first week

--- Week1/CSI_week_1/app/src/main/java/com/maxaltena/csi/ReportActivity.java
+++ Week1/CSI_week_1/app/src/main/java/com/maxaltena/csi/ReportActivity.java
@@ -1,7 +1,9 @@
 package com.maxaltena.csi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ReportActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class ReportActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+    }
+
+    public void handleBackClick(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
