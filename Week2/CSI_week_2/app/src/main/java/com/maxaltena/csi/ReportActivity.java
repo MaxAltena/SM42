fe7bc101126c87a22e01ef737192ec0b7d0589aa
MaxAltena <Max@MaxAltena.com>
2019-02-16 12:25:42 +0100

    forgot to push, so week 2 is here

---
+++ Week2/CSI_week_2/app/src/main/java/com/maxaltena/csi/ReportActivity.java
@@ -0,0 +1,30 @@
+package com.maxaltena.csi;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ReportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report);
+    }
+
+    public void handleBackClick(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void handleCameraClick(View view) {
+        Intent intent = new Intent(this, CameraActivity.class);
+        startActivity(intent);
+    }
+
+    public void handleLocationClick(View view){
+        Intent intent = new Intent(this, LocationActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ Week2/README.md
@@ -0,0 +1,7 @@
+# Week 2
+
+Deze week is er het volgende gedaan:
+
+- Nog niks
+
+Zoek je nog meer? Bezoek dan even [mijn portfolio website](https://portfolio.maxaltena.com/SM42/)!
