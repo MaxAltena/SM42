3199ba930e60b917968ec55ed7c97a23d3e0461f
MaxAltena <Max@MaxAltena.com>
2019-02-17 20:56:11 +0100

    added snaps

---
+++ Week2/snaps/app/src/main/java/com/maxaltena/snaps/MainActivity.java
@@ -0,0 +1,87 @@
+package com.maxaltena.snaps;
+
+import android.Manifest;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.os.Environment.getExternalStoragePublicDirectory;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnTakePicture;
+    ImageView imageView;
+
+    String pathToFile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnTakePicture = findViewById(R.id.takeButton);
+        if(Build.VERSION.SDK_INT >= 23){
+            requestPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }
+        btnTakePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchPictureTakerAction();
+            }
+        });
+        imageView = findViewById(R.id.imageView_picture);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            if(requestCode == 0){
+                Bitmap bitmap = BitmapFactory.decodeFile(pathToFile);
+                imageView.setImageBitmap(bitmap);
+            }
+        }
+    }
+
+    private void dispatchPictureTakerAction() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = createPhotoFile();
+            if(photoFile!= null ) {
+                pathToFile = photoFile.getAbsolutePath();
+                Uri photoURI = FileProvider.getUriForFile(MainActivity.this, "com.maxaltena.snaps.fileprovider", photoFile);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(intent,0);
+            }
+        }
+    }
+
+    private File createPhotoFile() {
+        String name = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File storageDir = getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = null;
+        try {
+            image = File.createTempFile(name, ".jpg", storageDir);
+        } catch (IOException e) {
+            Log.d("log", "Exception: " + e.toString());
+        }
+        return image;
+    }
+}
