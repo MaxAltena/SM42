de06b65729b6982d8510361391143be8b6eafbc8
MaxAltena <Max@MaxAltena.com>
2019-02-17 19:49:34 +0100

    added maps and working calling

--- Week2/CSI_week_2/app/src/main/java/com/maxaltena/csi/LocationActivity.java
+++ Week2/CSI_week_2/app/src/main/java/com/maxaltena/csi/LocationActivity.java
@@ -1,5 +1,16 @@
 package com.maxaltena.csi;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -10,10 +21,18 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import static android.Manifest.*;
+
 public class LocationActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
+    private double Lat;
+    private double Lng;
+
+    @TargetApi(Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,8 +41,62 @@ public class LocationActivity extends FragmentActivity implements OnMapReadyCall
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Lat = location.getLatitude();
+                Lng = location.getLongitude();
+
+                // Add a marker in Sydney and move the camera
+                LatLng currentLocation = new LatLng(Lat, Lng);
+                mMap.addMarker(new MarkerOptions().position(currentLocation).title("Current location"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(this, permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{
+                    permission.ACCESS_FINE_LOCATION
+            }, 10);
+            return;
+        } else {
+            getLocation();
+        }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 10:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    getLocation();
+                return;
+        }
+    }
+
+    private void getLocation() {
+        locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+    }
 
     /**
      * Manipulates the map once available.
@@ -39,8 +112,8 @@ public class LocationActivity extends FragmentActivity implements OnMapReadyCall
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng location = new LatLng(Lat, Lng);
+        mMap.addMarker(new MarkerOptions().position(location).title("Current location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
     }
 }
--- Week2/CSI_week_2/app/src/main/java/com/maxaltena/csi/ReportActivity.java
+++ Week2/CSI_week_2/app/src/main/java/com/maxaltena/csi/ReportActivity.java
@@ -1,9 +1,12 @@
 package com.maxaltena.csi;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import java.net.HttpCookie;
 
 public class ReportActivity extends AppCompatActivity {
 
@@ -23,8 +26,14 @@ public class ReportActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void handleLocationClick(View view){
+    public void handleLocationClick(View view) {
         Intent intent = new Intent(this, LocationActivity.class);
+        startActivity(intent);
+    }
+
+    public void handleCallClick(View view) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:112"));
         startActivity(intent);
     }
 }
